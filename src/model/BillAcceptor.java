cce6c3123dfd98ed00c8f469b444666634f5b3ee
neis <neistopx@gmail.com>
2025-04-18 18:13:26 +0600

    added bill acceptor

---
+++ src/model/BillAcceptor.java
@@ -0,0 +1,17 @@
+package model;
+
+public class CardAcceptor {
+    private int amount;
+
+    public CardAcceptor(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
